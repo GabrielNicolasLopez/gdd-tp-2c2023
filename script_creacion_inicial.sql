7483a0e3db28924d611350a5b93356eedacf5544
jsuh <jsuh@hexacta.com>
2023-09-30 16:41:24 -0300

    feat: definición de regiones de código

--- script_creacion_inicial.sql
+++ script_creacion_inicial.sql
@@ -1,6 +1,7 @@
 USE [GD2C2023]
 GO
 
+-- Inicio Configurar reglas de nombre de objetos
 SET ANSI_NULLS ON
 GO
 
@@ -9,10 +10,58 @@ GO
 
 SET CONCAT_NULL_YIELDS_NULL ON
 GO
+-- Fin Configurar reglas de nombre de objetos
 
+-- Inicio DROP FKs
+
+-- Fin DROP FKs
+
+-- Inicio DROP tablas
+
+-- Fin DROP tablas
+
+-- Inicio DROP Funciones
+
+-- Fin DROP Funciones
+
+-- Inicio DROP Procedimientos
+IF OBJECT_ID('LOS_HEREDEROS_DE_MONTIEL_Y_EL_DATO_PERSISTIDO.MIGRAR_EJEMPLO_SP') IS NOT NULL
+  DROP PROCEDURE LOS_HEREDEROS_DE_MONTIEL_Y_EL_DATO_PERSISTIDO.MIGRAR_EJEMPLO_SP
+GO
+-- Fin DROP Procedimientos
+
+-- Inicio crear schema de la aplicación
 IF EXISTS (SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = 'LOS_HEREDEROS_DE_MONTIEL_Y_EL_DATO_PERSISTIDO')
     DROP SCHEMA LOS_HEREDEROS_DE_MONTIEL_Y_EL_DATO_PERSISTIDO
 GO
 
 CREATE SCHEMA LOS_HEREDEROS_DE_MONTIEL_Y_EL_DATO_PERSISTIDO
 GO
+-- Fin crear schema de la aplicación
+
+-- Inicio crear tablas
+
+-- Fin crear tablas
+
+-- Inicio crear FKs
+
+-- Fin crear FKs
+
+-- Inicio crear Funciones
+
+-- Fin crear Funciones
+
+-- Inicio crear Procedimientos
+CREATE PROCEDURE LOS_HEREDEROS_DE_MONTIEL_Y_EL_DATO_PERSISTIDO.MIGRAR_EJEMPLO_SP
+AS
+BEGIN
+    -- Cuerpo
+    SELECT 1
+END
+GO
+-- Fin crear Procedimientos
+
+-- Inicio Invocaciones de procedimientos
+EXEC LOS_HEREDEROS_DE_MONTIEL_Y_EL_DATO_PERSISTIDO.MIGRAR_EJEMPLO_SP
+GO
+-- Fin Invocaciones de procedimientos
